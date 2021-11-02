7b0ec6258926e13a2263fdf55a749143da1b88e7
vmatzar <elmatzar123@gmail.com>
2021-11-01 20:52:28 -0600

    function finished, global configuration added, package json modified

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const messages = [
+    "Belice",
+    "Costa Rica",
+    "El Salvador",
+    "Guatemala",
+    "Honduras",
+    "Nicaragua",
+    "Panamá"
+];
+
+const randomMsg = () =>{
+    const message = messages[Math.floor(Math.random() * messages.length)];
+    console.log(message)
+}
